a27bbb24de0443b1245b45b1a0e2524875046e0c
TheNova22 <jayantsogikar@gmail.com>
2021-09-19 01:23:26 +0530

    Edited code

--- mdMaker.py
+++ mdMaker.py
@@ -5,6 +5,6 @@ def mdMaker(connections : dict):
     for k,v in connections.items():
         # print(1)
         mdFile.new_header(level=1,title=k)
-        mdFile.new_paragraph(Html.image(path=v, size='200'))
+        mdFile.new_paragraph(Html.image(path=v))
     mdFile.create_md_file()
     
