83b40b307da1af61b437791875d1f4961a14d7ba
TheNova22 <jayantsogikar@gmail.com>
2021-09-19 13:22:27 +0530

    Made markdown into a table

--- mdMaker.py
+++ mdMaker.py
@@ -2,10 +2,12 @@ def mdMaker(connections : dict):
     from mdutils.mdutils import MdUtils
     from mdutils import Html
     mdFile = MdUtils(file_name='Stickers',title='All Stickers')
+    texts = []
+    mdFile.new_line()
     for k,v in connections.items():
         # print(1)
-        mdFile.new_header(level=1,title=k)
-        mdFile.new_paragraph(Html.image(path=v))
-        mdFile.new_line()
+        texts.append(k)
+        texts.append(Html.image(path=v))
+    mdFile.new_table(text=texts, text_align='center', columns = 2,rows = len(texts) // 2)
     mdFile.create_md_file()
     
