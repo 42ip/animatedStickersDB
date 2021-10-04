0e5e715bb1e4f06a76a93b895b7dc9d6c99b72e1
TheNova22 <jayantsogikar@gmail.com>
2021-10-04 18:17:17 +0530

    Updated links

--- mdMaker.py
+++ mdMaker.py
@@ -7,7 +7,7 @@ def mdMaker(connections : dict):
     for _,v in connections.items():
         # print(1)
         texts.append(" or ".join(v[:-1]))
-        texts.append(Html.image(path=v[-1]))
+        texts.append(Html.image(path=v[-1], size='x100'))
     mdFile.new_table(text=texts, text_align='center', columns = 2,rows = len(texts) // 2)
     mdFile.create_md_file()
     print("done")
